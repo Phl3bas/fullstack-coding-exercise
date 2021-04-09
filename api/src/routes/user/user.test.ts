7c096146eebb023c75633a7b167c389ac4f72f10
Sepp Wikramaratna-Clarke <Phl3bas87@gmail.com>
2021-04-10 00:17:14 +0100

    api - usertest - it returns a list of users

--- api/src/routes/user/user.test.ts
+++ api/src/routes/user/user.test.ts
@@ -15,4 +15,13 @@ describe("user router", () => {
   it("should return content type of json", async () => {
     return await request(app).get("/api/user").expect("Content-Type", /json/);
   });
+
+  it("should return all users", async () => {
+    const res = await request(app).get("/api/user");
+    const body: User[] = await res.body;
+    expect(body.length).toBeGreaterThan(0);
+    expect(body[0]).toHaveProperty("name");
+    expect(body[0]).toHaveProperty("email");
+    expect(body[0]).toHaveProperty("location");
+  });
 });
