7e97ef79ba28bacf35990d1469c4be700ec75605
Sepp Wikramaratna-Clarke <Phl3bas87@gmail.com>
2021-04-11 00:30:25 +0100

    add material ui styling

--- client/src/components/filter/index.tsx
+++ client/src/components/filter/index.tsx
@@ -1,18 +1,54 @@
-import { TextField } from "@material-ui/core";
+import {
+  FormHelperText,
+  makeStyles,
+  TextField,
+  useTheme,
+} from "@material-ui/core";
 import * as React from "react";
 
 interface FilterProps {
   handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
+const useStyles = makeStyles((theme) => ({
+  form: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  filter: {
+    width: "50%",
+    margin: "0 auto",
+    marginBottom: "20px",
+    backgroundColor: "rgba(244,244,244,.1)",
+    color: theme.palette.primary.dark,
+    "&::placeholder": {
+      color: "red",
+    },
+  },
+}));
+
 export const Filter = ({ handleChange }: FilterProps) => {
+  const theme = useTheme();
+  const classes = useStyles();
   return (
-    <form onSubmit={(e) => e.preventDefault()}>
+    <form onSubmit={(e) => e.preventDefault()} className={classes.form}>
       <TextField
+        className={classes.filter}
         onChange={handleChange}
         id="outlined-basic"
         label="Filter users"
-        variant="outlined"
+        InputProps={{
+          style: {
+            color: theme.palette.primary.light,
+          },
+        }}
+        InputLabelProps={{
+          style: {
+            color: theme.palette.primary.light,
+          },
+        }}
+        variant="filled"
       />
     </form>
   );
