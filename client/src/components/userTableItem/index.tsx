4fc82ae6036d0027fb62a399daad68aa5d38376e
Sepp Wikramaratna-Clarke <45594209+Phl3bas@users.noreply.github.com>
2021-04-11 00:32:00 +0100

    Make it look nice (#7)
    * lerna init
    
    * add postinstall script
    
    * add .gitignore
    
    * api - add express deps
    
    * api - add nodemon and dev script
    
    * api - add tsconfig
    
    * api - mv sampledata to db folder
    
    * mv sampledata to db folder
    
    * api - create http server
    
    * api dev scripts
    
    * api - add global middleares
    
    * api - add global middlewares
    
    * api - add user route
    
    * api - userRouter - some delightful comments
    
    * api - remove useless import
    
    * api - testing deps
    
    * api - test deps
    
    * api - jest config for typescript
    
    * api - user router test statuscode
    
    * api - usertest - json response
    
    * api - usertest - it returns a list of users
    
    * api - scripts update
    
    * api - update scripts
    
    * client - add usertable component
    
    * client - create usertableitem component
    
    * client - fetch users w/hook
    
    * client - create usefliteruserlist hook
    
    * client - add list flitering
    
    * add material ui styling

--- client/src/components/userTableItem/index.tsx
+++ client/src/components/userTableItem/index.tsx
@@ -1,4 +1,4 @@
-import { TableCell, TableRow } from "@material-ui/core";
+import { makeStyles, TableCell, TableRow } from "@material-ui/core";
 import * as React from "react";
 
 interface UserTableItemProps {
@@ -7,16 +7,26 @@ interface UserTableItemProps {
   location: string;
 }
 
+const useStyle = makeStyles((theme) => ({
+  cell: {
+    borderBottomColor: "rgba(0, 0, 0, 0.1)",
+    color: "rgba(255,255,255,.8)",
+  },
+}));
+
 export const UserTableItem = ({
   name,
   email,
   location,
 }: UserTableItemProps) => {
+  const classes = useStyle();
   return (
-    <TableRow>
-      <TableCell>{name}</TableCell>
-      <TableCell>{email}</TableCell>
-      <TableCell>{location}</TableCell>
+    <TableRow hover>
+      <TableCell component="th" scope="row" className={classes.cell}>
+        {name}
+      </TableCell>
+      <TableCell className={classes.cell}>{email}</TableCell>
+      <TableCell className={classes.cell}>{location}</TableCell>
     </TableRow>
   );
 };
